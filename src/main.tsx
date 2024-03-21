e43316516125074755e7e285eff74f9e64fbd894
Philipp <philipp@geuer-pollmann.de>
2024-03-21 10:38:28 +0100

    break: changed to vite + react

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { applyPolyfills, defineCustomElements } from '@telekom/scale-components/loader'
+import App from './App.tsx'
+import './index.css'
+import '@telekom/scale-components/dist/scale-components/scale-components.css';
+
+applyPolyfills().then(() => {
+    defineCustomElements(window);
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
